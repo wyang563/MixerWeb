39adb8fef5430244006cb646457ffef698db7b42
Andrew Zhao <90993641+azhao25@users.noreply.github.com>
2022-01-15 18:03:44 -0500

    updated sign up page a lil

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
  
 const SignUp = () => {
@@ -12,5 +12,43 @@ const SignUp = () => {
         </div>
     )
 }
+
+export default SignUp;
+*/
+import React from "react";
+import "../../utilities.css";
+
+const UsersList = (props) => {
+  const [allUsers, setAllUsers] = useState([]);
+};
+
+const SignUp = () => {
+  return (
+    <html>
+      <head>
+        <h1 className="u-textCenter"> Sign Up</h1>
+      </head>
+      <body className="u-textCenter">
+        <form>
+          Name:
+          <input type="text" name="Name" />
+        </form>
+        <form>
+          Username:
+          <input type="text" name="Username" />
+        </form>
+        <form>
+          Email Address:
+          <input type="text" name="Email Address" />
+        </form>
+        <form>
+          Password:
+          <input type="text" name="Password" />
+        </form>
+        <button> SUBMIT </button>
+      </body>
+    </html>
+  );
+};
 
 export default SignUp;
