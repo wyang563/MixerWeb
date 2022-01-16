575896670c9360226e5b5ace4c467df96675d7a3
wyang563 <86643142+wyang563@users.noreply.github.com>
2022-01-15 22:43:45 -0500

    Update SignUp.js

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,44 +1,49 @@
-import React from "react";
-import "../../utilities.css";
+import React from 'react';
 
-const UsersList = (props) => {
-  const [allUsers, setAllUsers] = useState([]);
-};
-const NewSignUp = {
-  Name: "",
-  Username: "",
-  EmailAddress: "",
-  Password: "",
-};
+export default class FormSubmission extends React.Component {
+    constructor (props){
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            userName: "",
+            email: "",
+            password: "",
+        }
+    }
+    handleInputChanged(event) {
+        const value = event.target.value;
+        this.setState({
+            [event.target.name]: value
+        });
+    }
+    handleButtonClicked(event) {
+        alert("submission completed "+ this.state.value);
+        event.preventDefault();
+    }
+    render () {
+        return (
+            <div>
+                <h1>Sign Up Form</h1>
+                <form>
+                    <label>First Name</label>
+                    <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleInputChanged.bind(this)} />
+                    <label>Last Name</label>
+                    <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleInputChanged.bind(this)} />
 
-const SignUp = () => {
-  return (
-    <html>
-      <head>
-        <h1 className="u-textCenter"> Sign Up</h1>
-      </head>
-      <body className="u-textCenter">
-        <form>
-          Name:
-          <input type="text" name="Name" />
-        </form>
-        <form>
-          Username:
-          <input type="text" name="Username" />
-        </form>
-        <form>
-          Email Address:
-          <input type="text" name="Email Address" />
-        </form>
-        <form>
-          Password:
-          <input type="text" name="Password" />
-        </form>
-        <button> SUBMIT </button>
-      </body>
-    </html>
-  );
-};
+                    <label>Username</label>
+                    <input type="text" name="userName" value={this.state.userName} onChange={this.handleInputChanged.bind(this)} />
+                    <label>Edu Email Address</label>
+                    <input type="email" name="email" value={this.state.email} onChange={this.handleInputChanged.bind(this)} />
+                    <label> Password: </label>
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleInputChanged.bind(this)} />
+                    <input type="submit" value="Submit" onClick={this.handleButtonClicked()} />
+                </form>
+                
+            </div>
+        )
+    }
+    
+}
 
-export default SignUp;
-export var NewSignUp;
+
