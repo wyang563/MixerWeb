9bd7475655cf730552a94b2e101c67ade87718e4
Bryant Xia <bryantxia2435@gmail.com>
2022-01-17 14:02:33 -0500

    emailing new sign ups

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import authenticateUser from '../authenticateUser.js';
+import sendSignInLinkToEmail from "../auth_email_link_send.js"
+import { getAuth } from "firebase/auth";
+
+import "./SignUp.css";
 
 export default class SignUp extends React.Component {
     constructor (props){
@@ -26,35 +30,54 @@ export default class SignUp extends React.Component {
         }
         else{
             alert("Please confirm your email address");
-            authenticateUser(this.state.email, this.state.password, false);
-        }
-        
+            //authenticateUser(this.state.email, this.state.password, false);
+            const auth = getAuth();
+            const actionCodeSettings = {
+                // URL you want to redirect back to. The domain (www.example.com) for this
+                // URL must be in the authorized domains list in the Firebase Console.
+                url: 'http://localhost:5000/',
+                // This must be true.
+                handleCodeInApp: true,
+                iOS: {
+                  bundleId: 'com.example.ios'
+                },
+                android: {
+                  packageName: 'com.example.android',
+                  installApp: true,
+                  minimumVersion: '12'
+                },
+                dynamicLinkDomain: 'mixermit.page.link'
+              };
+
+            sendSignInLinkToEmail (auth, this.state.email, actionCodeSettings)
+        } 
         event.preventDefault();
     }
     
     render () {
         return (
-            <div>
+            <div className="centering_everything">
                 <h1>Sign Up Form</h1>
-                <form>
-                    <label>First Name</label>
-                    <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleInputChanged.bind(this)} />
-                    <label>Last Name</label>
-                    <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleInputChanged.bind(this)} />
-
-                    <label>Username</label>
-                    <input type="text" name="userName" value={this.state.userName} onChange={this.handleInputChanged.bind(this)} />
-                    <label>Edu Email Address</label>
-                    <input type="email" name="email" value={this.state.email} onChange={this.handleInputChanged.bind(this)} />
+                <form> 
+                    <label>First Name:  </label>
+                    <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleInputChanged.bind(this)}/>
+                    <br></br>
+                    <label>Last Name:  </label>
+                    <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleInputChanged.bind(this)}/>
+                    <br></br>
+                    <label>Username:  </label>
+                    <input type="text" name="userName" value={this.state.userName} onChange={this.handleInputChanged.bind(this)}/>
+                    <br></br>
+                    <label>@mit.edu email address: </label>
+                    <input type="email" name="email" value={this.state.email} onChange={this.handleInputChanged.bind(this)}/>
+                    <br></br>
                     <label> Password: </label>
-                    <input type="password" name="password" value={this.state.password} onChange={this.handleInputChanged.bind(this)} />
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleInputChanged.bind(this)}/>
                 </form>
                 <button onClick={this.handleButtonClicked.bind(this)}>Submit</button>
-                
             </div>
         )
     }
-    
 }
 
  
